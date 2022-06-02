b65e20222807ccaa97fc771e78b8ea4245b961bb
Francisco Carneiro <87398075+Funeto@users.noreply.github.com>
2022-06-01 22:56:50 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+function insert(num){
+    document.form.visor.value = document.form.visor.value + num;
+}
+function equal(){
+    exp = document.form.visor.value;
+    if(exp){
+       document.form.visor.value = eval(exp);
+}
+}
+function c(){
+    document.form.visor.value = "";
+}
+function back(){
+    var exp = document.form.visor.value;
+    document.form.visor.value = exp.substring(0, exp.length-1);
+}
